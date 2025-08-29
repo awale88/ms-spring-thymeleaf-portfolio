0bc3b2f9aa60e81b441b7773e6cfc4b03f704819
nayanawale <awalenayan11@gmail.com>
2025-08-29 14:09:27 +0100

    configured swagger-ui

--- src/main/java/com/spring/portfolio/config/SwaggerConfig.java
+++ src/main/java/com/spring/portfolio/config/SwaggerConfig.java
@@ -1,8 +1,13 @@
 package com.spring.portfolio.config;
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class SwaggerConfig {
@@ -13,6 +18,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Portfolio Contact API")
                         .version("1.0")
-                        .description("API for contact forms and sending emails"));
+                        .description("API for contact forms and sending emails"))
+                .servers(List.of(
+                        new Server()
+                                .url("https://awale-portfolio.up.railway.app")
+                                .description("Production Server"),
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Local Server")));
     }
 }
