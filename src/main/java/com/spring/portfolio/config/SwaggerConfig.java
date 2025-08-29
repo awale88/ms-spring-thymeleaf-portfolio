6c43da044479b00a630ee99122f6a7cfac6595cc
nayanawale <awalenayan11@gmail.com>
2025-08-29 14:34:41 +0100

    updated url

--- src/main/java/com/spring/portfolio/config/SwaggerConfig.java
+++ src/main/java/com/spring/portfolio/config/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
                         .description("API for contact forms and sending emails"))
                 .servers(List.of(
                         new Server()
-                                .url("https://awale-portfolio.up.railway.app")
+                                .url("https://swagger-ui-production.up.railway.app/")
                                 .description("Production Server"),
                         new Server()
                                 .url("http://localhost:8080")
